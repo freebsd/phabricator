0e3317563def3324be7144dcef98291b673e5149
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-09-11 10:53:34 +0000

    [FreeBSD] Add a note for activating account

--- src/applications/auth/controller/PhabricatorAuthRegisterController.php
+++ src/applications/auth/controller/PhabricatorAuthRegisterController.php
@@ -630,6 +630,15 @@ final class PhabricatorAuthRegisterController
             'authentication mechanisms later on.'));
     }
 
+    $freebsd_view = id(new PHUIInfoView())
+      ->setSeverity(PHUIInfoView::SEVERITY_NOTICE)
+      ->setTitle(pht('Note to new registration'))
+      ->appendChild(
+        pht(
+          'Due to spam, after creating new account, please sent an email to '.
+          '<phabric-admin AT FreeBSD.org> with your email address and '.
+          'briefly describe your plan to get your account activated.'));
+
     $object_box = id(new PHUIObjectBoxView())
       ->setForm($form)
       ->setFormErrors($errors);
@@ -647,6 +656,7 @@ final class PhabricatorAuthRegisterController
       ->setFooter(
         array(
           $welcome_view,
+          $freebsd_view,
           $invite_header,
           $object_box,
         ));
